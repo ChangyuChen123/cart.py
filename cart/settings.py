a50efef21db5e15bafe371f40a8cc318329d5c7a
ChangyuChen123 <120720474+ChangyuChen123@users.noreply.github.com>
2023-04-23 17:12:50 +0800

    更新setting

--- cart/settings.py
+++ cart/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yg3@(tb*d#@*oq+v)2=h9w%dsc66_**^)&3zik$x8gbl)pbx@5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
